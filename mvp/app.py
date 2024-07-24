2ced9ea029d18812bb3767ed7cc644a42351bde8
Andre Gigena <agigena@berkeley.edu>
2024-07-23 20:47:52 -0700

    Removing about page

--- mvp/app.py
+++ mvp/app.py
@@ -3,8 +3,7 @@ import streamlit as st
 home_page = st.Page("home.py", title="Home", icon="🏚️")
 data_input_page = st.Page("data_form.py", title="Enter Intake Data", icon="📝")
 data_api_page = st.Page("data_api.py", title="Generate Report", icon="📊")
-about_page = st.Page("about.py", title="About/Help", icon="📖")
 
-pg = st.navigation([home_page, data_input_page, data_api_page, about_page])
+pg = st.navigation([home_page, data_input_page, data_api_page])
 st.set_page_config(page_title="Pawpulation Forecast", page_icon="🐶")
 pg.run()
