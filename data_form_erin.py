4e513261c91204be9e1a7a488b6cab33907d025e
robchimel <rchimel@ischool.berkeley.edu>
2024-07-30 19:59:31 -0700

    fixed data from, organized repo

--- data_form_erin.py
+++ data_form_erin.py
@@ -3,12 +3,11 @@ import pandas as pd
 import streamlit as st
 from dashboard_utils import MODEL_COLS, TIME_BIN_DICT
 import os
-import sys
 import pickle
 from utils import *
 
 FORM_DATA = {}
-
+prediction_text = ''
 st.markdown("""
 # Intake Data Entry
 Please input the requested intake data below. When finished, click the
@@ -16,53 +15,55 @@ Please input the requested intake data below. When finished, click the
 """)
 
 with st.form("intake_form"):
-    FORM_DATA[1] = st.text_input("What is their name?", None)
+    FORM_DATA['Name'] = st.text_input("What is their name?", None)
 
-    FORM_DATA[2] = st.selectbox("What is their type?",("Dog", "Cat"),None)
+    FORM_DATA['Type'] = st.selectbox("What is their type?",("Dog", "Cat"),None)
 
-    FORM_DATA[3] = st.text_input("What is their breed?", None)
+    FORM_DATA['Breed'] = st.text_input("What is their breed?", None)
 
-    FORM_DATA[4] = st.text_input("What is their color?", None)
+    FORM_DATA['Color'] = st.text_input("What is their color?", None)
 
-    FORM_DATA[5] = st.selectbox("What is their sex?", ("Female", "Male", "Unknown"), index=None)
+    FORM_DATA['Sex'] = st.selectbox("What is their sex?", ("Female", "Male", "Unknown"), index=None)
 
-    FORM_DATA[6] = st.selectbox("What is their size?", ("Small", "Medium", "Kittn", "Large", "Toy", "Puppy", "X-LRG", "Unknown"), index=None)
+    FORM_DATA['Size'] = st.selectbox("What is their size?", ("Small", "Medium", "Kittn", "Large", "Toy", "Puppy", "X-LRG", "Unknown"), index=None)
 
-    FORM_DATA[7] = st.date_input("When is their birthday?", value=None)
+    FORM_DATA['Date of Birth'] = st.date_input("When is their birthday?", value=None)
 
-    FORM_DATA[8] = st.text_input("What is their kennel number?", None)
+    FORM_DATA['Kennel Number'] = st.text_input("What is their kennel number?", 'DA21')
 
-    FORM_DATA[9] = st.date_input("When did they go through intake processing?", value=None)
+    FORM_DATA['Intake Date'] = st.date_input("When did they go through intake processing?", value=None)
 
-    FORM_DATA[10] = st.selectbox("What is their intake type?",
+    FORM_DATA['Days in Shelter'] = st.date_input("How many days have they been in the shelter?", value=35)
+
+    FORM_DATA['Intake Type'] = st.selectbox("What is their intake type?",
     ("Stray", "Owner Surrender", "Confiscate", "Quarantine", "Adoption Return", "Transfer", "Born Here", "Unknown"), index=None)
 
-    FORM_DATA[11] = st.selectbox("What is their intake subtype?",
+    FORM_DATA['Intake Subtype'] = st.selectbox("What is their intake subtype?",
     ("Field", "Over the counter", "Comm cat", "Fld_arrest", "phone", "vet_hosp", "fld_stray", "fld_hosptl", "priv_shelt", "born_here",
      "fld_coronr", "fld_cruel", "mun_shelt", "field_return to owner", "fld_evict", "field_os", "fld_aband", "email", "over the counter_os",
      "mom stray", "over the counter_return to owner", "over the counter_owned", "rescue_grp", "fld_invest", "over the counter_arrest", "fld_owned",
      "fld_livstk", "over the counter_coronr", "over the counter_evict", "mom os", "over the counter_emerge", "Unknown"),index=None)
 
-    FORM_DATA[12] = st.selectbox(
-    "What is their condition",
+    FORM_DATA['Intake Condition'] = st.selectbox(
+    "What is their condition?",
     ("Healthy", "Treatable/Rehab", "Untreatable", "Treatable/Manageable","Unknown"),index=None)
 
-    FORM_DATA[13] = st.selectbox(
+    FORM_DATA['Intake Jurisdiction'] = st.selectbox(
     "What is your shelter's jurisdiction?",
     ("Santa Rosa", "County", "Windsor", "Out of County", "Rohnert Park", "Healdsburg", "Sonoma", "Petaluma", "Cloverdale", "sebastopol",
      "tribal resv", "Cotati", "Unknown"),index=None)
 
     submitted = st.form_submit_button("Generate Prediction")
-
+st.form_submit_button()
 if submitted:
-    # Prediction pipeline
+    # Run data through data pipeline
     df = pd.DataFrame(FORM_DATA, index=[0])
-    cols = ['Name', 'Type', 'Breed', 'Color', 'Sex', 'Size', 'Date Of Birth',  
-        'Kennel Number', 'Intake Date', 'Intake Type', 'Intake Subtype', 
-        'Intake Condition', 'Intake Jurisdiction']
-    df.columns = cols
-    # Run df through data pipeline
-    params = {
+
+    if df.shape[0] == 0:
+        st.error("No data available for this animal. Please try again.")
+    else:
+        # Load model and generate prediction
+        params = {
         'na_data': 'fill',
         'drop_outlier_days': False,
         'embed':False,
@@ -73,22 +74,15 @@ if submitted:
             'train_size':0.6, 'validate_size':0.2, 'test_size':0.2
             }
         }
-    results_df = load_df(params, data=df, split_data=False)
-    # Load model and generate prediction
-    try:
+        df = load_df(params, data=df, split_data=False)
         if os.path.isfile(os.path.join(os.path.dirname(os.getcwd()),'XGBpipeline.pkl')):
             pipeline_path = os.path.join(os.path.dirname(os.getcwd()),'XGBpipeline.pkl')
         if os.path.isfile(os.path.join(os.getcwd(), 'XGBpipeline.pkl')):
             pipeline_path = os.path.join(os.getcwd(), 'XGBpipeline.pkl')
         with open(pipeline_path, 'rb') as file:
             XGBpipeline = pickle.load(file)
-            
         # Predict on the test data
-        _, features, _, _, _ = sklearn_pipeline(results_df, results_df)
-
-        results_df['Days_in_Shelter_Prediction'] = XGBpipeline.predict(features)
-        prediction_text = results_df.Days_in_Shelter_Prediction.iloc[0]
-        predict_los_string = {0:'0 to 3 days', 1:'3 to 14 days', 2:'14 to 30 days', 3:'30 to 100 days', 4:'100+ days'}
-        st.markdown(f"The animal is predicted to stay for {predict_los_string[prediction_text]}.")
-    except Exception as e:
-        st.markdown(f"Error with user input, prediction not returned.\n {e}")
+        _, features, _, _, _ = sklearn_pipeline(df, df)
+        prediction_text = XGBpipeline.predict(features)
+        
+st.markdown(f"The animal is predicted to stay for {prediction_text}.")
