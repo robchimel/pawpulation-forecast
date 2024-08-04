27f1f9f8a7d129b65c14e66bc5b888720d826b56
robchimel <rchimel@ischool.berkeley.edu>
2024-08-03 17:57:01 -0700

    default inputs

--- data_form.py
+++ data_form.py
@@ -5,6 +5,7 @@ from dashboard_utils import MODEL_COLS, TIME_BIN_DICT
 import os
 import sys
 import pickle
+from datetime import datetime
 from utils import *
 
 FORM_DATA = {}
@@ -16,41 +17,41 @@ Please input the requested intake data below. When finished, click the
 """)
 
 with st.form("intake_form"):
-    FORM_DATA[1] = st.text_input("What is their name?", "FIDO")
+    FORM_DATA[1] = st.text_input("What is their name?", placeholder="FIDO")
 
-    FORM_DATA[2] = st.selectbox("What is their type?",("Dog", "Cat"),"Dog")
+    FORM_DATA[2] = st.selectbox("What is their type?",("Dog", "Cat"),placeholder="Dog")
 
-    FORM_DATA[3] = st.text_input("What is their breed?", "GERM SHEPHERD")
+    FORM_DATA[3] = st.text_input("What is their breed?", placeholder="GERM SHEPHERD")
 
-    FORM_DATA[4] = st.text_input("What is their color?", "TAN/BLACK")
+    FORM_DATA[4] = st.text_input("What is their color?", placeholder="TAN/BLACK")
 
-    FORM_DATA[5] = st.selectbox("What is their sex?", ("Female", "Male", "Unknown"), index="Male")
+    FORM_DATA[5] = st.selectbox("What is their sex?", ("Female", "Male", "Unknown"), placeholder="Male")
 
-    FORM_DATA[6] = st.selectbox("What is their size?", ("Small", "Medium", "Kittn", "Large", "Toy", "Puppy", "X-LRG", "Unknown"), index="Large")
+    FORM_DATA[6] = st.selectbox("What is their size?", ("Small", "Medium", "Kittn", "Large", "Toy", "Puppy", "X-LRG", "Unknown"), placeholder="Small")
 
-    FORM_DATA[7] = st.date_input("When is their birthday?", datetime.date(2022, 5, 11))
+    FORM_DATA[7] = st.date_input("When is their birthday?", None)
 
-    FORM_DATA[8] = st.text_input("What is their kennel number?", "DA31")
+    FORM_DATA[8] = st.text_input("What is their kennel number?", placeholder="DA31")
 
-    FORM_DATA[9] = st.date_input("When did they go through intake processing?", datetime.date(2024, 1, 21))
+    FORM_DATA[9] = st.date_input("When did they go through intake processing?", None)
 
     FORM_DATA[10] = st.selectbox("What is their intake type?",
-    ("Stray", "Owner Surrender", "Confiscate", "Quarantine", "Adoption Return", "Transfer", "Born Here", "Unknown"), index="Owner Surrender")
+    ("Stray", "Owner Surrender", "Confiscate", "Quarantine", "Adoption Return", "Transfer", "Born Here", "Unknown"), placeholder="Owner Surrender")
 
     FORM_DATA[11] = st.selectbox("What is their intake subtype?",
     ("Field", "Over the counter", "Comm cat", "Fld_arrest", "phone", "vet_hosp", "fld_stray", "fld_hosptl", "priv_shelt", "born_here",
      "fld_coronr", "fld_cruel", "mun_shelt", "field_return to owner", "fld_evict", "field_os", "fld_aband", "email", "over the counter_os",
      "mom stray", "over the counter_return to owner", "over the counter_owned", "rescue_grp", "fld_invest", "over the counter_arrest", "fld_owned",
-     "fld_livstk", "over the counter_coronr", "over the counter_evict", "mom os", "over the counter_emerge", "Unknown"),index="Over the counter")
+     "fld_livstk", "over the counter_coronr", "over the counter_evict", "mom os", "over the counter_emerge", "Unknown"),placeholder="Over the counter")
 
     FORM_DATA[12] = st.selectbox(
     "What is their condition",
-    ("Healthy", "Treatable/Rehab", "Untreatable", "Treatable/Manageable","Unknown"),index="Healthy")
+    ("Healthy", "Treatable/Rehab", "Untreatable", "Treatable/Manageable","Unknown"),placeholder="Healthy")
 
     FORM_DATA[13] = st.selectbox(
     "What is your shelter's jurisdiction?",
     ("Santa Rosa", "County", "Windsor", "Out of County", "Rohnert Park", "Healdsburg", "Sonoma", "Petaluma", "Cloverdale", "sebastopol",
-     "tribal resv", "Cotati", "Unknown"),index="County")
+     "tribal resv", "Cotati", "Unknown"),placeholder="County")
 
     submitted = st.form_submit_button("Generate Prediction")
 
